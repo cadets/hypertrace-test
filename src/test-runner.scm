cca30614d8496bb80254913ad0f0108c9f8e979f
Domagoj Stolfa <ds815@gmx.com>
2021-07-30 16:12:18 +0100

    hypertrace-test: bare-run-test: Add new public procedure.
    * src/test-runner.scm: Add bare-run-test, which simply asserts true with a test
    name. The intended use of this procedure is testing existence of tests in
    test-groups.

--- src/test-runner.scm
+++ src/test-runner.scm
@@ -9,6 +9,8 @@
 
 (import srfi-1)
 
+(import test)
+
 ;;
 ;; Reads a file line by line and returns a string with the contents
 ;; of that file.
@@ -26,6 +28,20 @@
 	   (loop (read-line fh) (cons* c "\n" lines)))))))
 
 
+;;
+;; Do a bare run of a test. This is simply used to check that the test is
+;; sensibly specified within a certain test suite and can be used on any
+;; operating system. Running actual tests requires HyperTrace to be working
+;; and thus can't be run on just any machine.
+;;
+
+(define (bare-run-test test)
+  (let* ((name         (hypertrace-test-name test))
+	 (in-file      (hypertrace-test-in-file test))
+	 (expected-out (hypertrace-test-expected-out test))
+	 (cmp-method   (hypertrace-test-cmp-method test))
+	 (run-method   (hypertrace-test-run-method test)))
+    (test-assert name #t)))
 ;;
 ;; TODO(dstolfa):
 ;; Runs a test specified as a hypertrace-test record.
