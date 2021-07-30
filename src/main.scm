9c770f5d7c90ca1f7b662eba1ec4d25d5db93384
Domagoj Stolfa <ds815@gmx.com>
2021-07-30 16:30:34 +0100

    hypertrace-test: main: Add procedure.
    * src/main.scm: Add a new 'main' procedure which tages in command line
    arguments.

--- src/main.scm
+++ src/main.scm
@@ -1,57 +1,63 @@
-(import test)
+
+(import (test)
+	(chicken process-context)
+	(args))
 
 (declare (uses hypertrace-test-runner))
 (declare (uses hypertrace-test))
 
 
-(test-group "Field tests"
-	    (define test-test (mk-hypertrace-test
-			       '((name "Foo")
-				 (expected-out "Bar"))))
+(define (main args)
+  (test-group "Field tests"
+	      (define test-test (mk-hypertrace-test
+				 '((name "Foo")
+				   (expected-out "Bar"))))
 
-	    (test "Foo" (hypertrace-test-name test-test))
-	    (test "Bar" (hypertrace-test-expected-out test-test))
-	    (test #f (hypertrace-test-in-file test-test))
-	    (test 'default-cmp-method (hypertrace-test-cmp-method test-test))
-	    (test 'default-run-method (hypertrace-test-run-method test-test)))
+	      (test "Foo" (hypertrace-test-name test-test))
+	      (test "Bar" (hypertrace-test-expected-out test-test))
+	      (test #f (hypertrace-test-in-file test-test))
+	      (test 'default-cmp-method (hypertrace-test-cmp-method test-test))
+	      (test 'default-run-method (hypertrace-test-run-method test-test)))
 
-(test-group "Unbound inputs"
-	    (test '(#f "Field hypertrace-test-unbound-symbol is not bound.")
-		  (mk-hypertrace-test '((unbound-symbol "FAIL"))))
-	    (test '(#f "Field hypertrace-test-unbound-symbol is not bound.")
-		  (mk-hypertrace-test
-		   '((name "foo")
-		     (unbound-symbol "FAIL"))))
-	    (test '(#f "Field hypertrace-test-unbound-symbol is not bound.")
-		  (mk-hypertrace-test
-		   '((unbound-symbol "FAIL")
-		     (name "foo")))))
+  (test-group "Unbound inputs"
+	      (test '(#f "Field hypertrace-test-unbound-symbol is not bound.")
+		    (mk-hypertrace-test '((unbound-symbol "FAIL"))))
+	      (test '(#f "Field hypertrace-test-unbound-symbol is not bound.")
+		    (mk-hypertrace-test
+		     '((name "foo")
+		       (unbound-symbol "FAIL"))))
+	      (test '(#f "Field hypertrace-test-unbound-symbol is not bound.")
+		    (mk-hypertrace-test
+		     '((unbound-symbol "FAIL")
+		       (name "foo")))))
 
-(test-group "Non-symbols"
-	    (test '(#f "Field string is not a symbol.")
-		  (mk-hypertrace-test '(("string" "FAIL"))))
-	    (test '(#f "Field string is not a symbol.")
-		  (mk-hypertrace-test
-		   '((name "foo")
-		     ("string" "FAIL"))))
-	    (test '(#f "Field string is not a symbol.")
-		  (mk-hypertrace-test
-		   '(("string" "FAIL")
-		     (name "foo")))))
+  (test-group "Non-symbols"
+	      (test '(#f "Field string is not a symbol.")
+		    (mk-hypertrace-test '(("string" "FAIL"))))
+	      (test '(#f "Field string is not a symbol.")
+		    (mk-hypertrace-test
+		     '((name "foo")
+		       ("string" "FAIL"))))
+	      (test '(#f "Field string is not a symbol.")
+		    (mk-hypertrace-test
+		     '(("string" "FAIL")
+		       (name "foo")))))
 
 
-(define hypertrace-test-nonproc 'non-proc)
-(test-group "Non-procedures"
-	    (test `(#f ,(eval (string-append "Field hypertrace-test-nonproc is "
-				      "not a procedure in the environment.")))
-		  (mk-hypertrace-test '((nonproc "FAIL"))))
-	    (test `(#f ,(eval (string-append "Field hypertrace-test-nonproc is "
-				      "not a procedure in the environment.")))
-		  (mk-hypertrace-test '((name "foo")
-					(nonproc "FAIL"))))
-	    (test `(#f ,(eval (string-append "Field hypertrace-test-nonproc is "
-				      "not a procedure in the environment.")))
-		  (mk-hypertrace-test '((nonproc "FAIL")
-					(name "foo")))))
+  (define hypertrace-test-nonproc 'non-proc)
+  (test-group "Non-procedures"
+	      (test `(#f ,(eval (string-append "Field hypertrace-test-nonproc is "
+					       "not a procedure in the environment.")))
+		    (mk-hypertrace-test '((nonproc "FAIL"))))
+	      (test `(#f ,(eval (string-append "Field hypertrace-test-nonproc is "
+					       "not a procedure in the environment.")))
+		    (mk-hypertrace-test '((name "foo")
+					  (nonproc "FAIL"))))
+	      (test `(#f ,(eval (string-append "Field hypertrace-test-nonproc is "
+					       "not a procedure in the environment.")))
+		    (mk-hypertrace-test '((nonproc "FAIL")
+					  (name "foo")))))
 
-(bare-run-test (mk-hypertrace-test '((name "Bare-run test"))))
+  (bare-run-test (mk-hypertrace-test '((name "Bare-run test")))))
+
+(main command-line-arguments)
