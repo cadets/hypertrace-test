9c6379afc7079bb245050fe0bb1477551837f67a
Domagoj Stolfa <ds815@gmx.com>
2021-08-02 15:19:36 +0100

    hypertrace-test: hypertrace-options: Add usage and fix spelling.
    * src/options.scm: Add a way to print help text for the program and capitalize
    the text for each of the options.

--- src/options.scm
+++ src/options.scm
@@ -1,15 +1,30 @@
 (declare (unit hypertrace-option-parser))
 
 (import scheme
+	(chicken process-context)
+	(chicken port)
 	args)
+
+(define (usage)
+  (with-output-to-port (current-error-port)
+    (lambda ()
+      (print "Usage: " (car (argv)) " [options...]")
+      (newline)
+      (print (args:usage hypertrace-options))
+      (print "Report bugs to <ds815@gmx.com> or <ds815@cam.ac.uk>")))
+  (exit 1))
 
 (define hypertrace-options
   (list
    (args:make-option
-    (b bare)    #:none              "bare run of tests"
+    (b bare)    #:none              "Bare run of the test suite"
     (display "Running tests in bare mode...\n"))
    
    (args:make-option
-    (V verbose) (optional: "LEVEL") "verbosity level [default: 1]"
-    (set! arg (string->number (or arg "1"))))))
+    (V verbose) (optional: "LEVEL") "Verbosity level [default: 1]"
+    (set! arg (string->number (or arg "1"))))
+
+   (args:make-option
+    (h help) #:none                 "Display this help text"
+    (usage))))
 
