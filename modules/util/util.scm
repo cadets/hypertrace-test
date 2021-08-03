40fc5910b7e56442e91898777289edf18d94f410
Domagoj Stolfa <ds815@gmx.com>
2021-08-03 18:00:37 +0100

    hypertrace-test: module (util): Add documentation.
    * modules/util/util.scm: Document the macros.

--- modules/util/util.scm
+++ modules/util/util.scm
@@ -4,6 +4,13 @@
 			   with-environment-variable-if-not-f)
   (import scheme)
   (import-for-syntax (chicken process-context))
+
+
+  ;;
+  ;; Run the specified command with an environment variable set to value.
+  ;; After finishing, the environment variable is restored to what it was before
+  ;; thus giving a declarative way to set environment variables.
+  ;;
   
   (define-syntax with-environment-variable
     (er-macro-transformer
@@ -18,6 +25,12 @@
 	    (,(r 'set-environment-variable!) ,variable current)
 	    (,(r 'unset-environment-variable!) ,variable)))))))
 
+
+  ;;
+  ;; Similar to with-environment-variable, however it only sets the variable if
+  ;; it hasn't been set already (either by the environment or the user) calling
+  ;; the program.
+  ;;
 
   (define-syntax with-environment-variable-if-not-f
     (er-macro-transformer
