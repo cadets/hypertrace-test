9bae0ec60e3af0338251eb363100af5c397a201e
Domagoj Stolfa <ds815@gmx.com>
2021-08-06 21:14:01 +0100

    hypertrace-test: test (begin-global-var): Add new test.
    * tests/elf-checksum/begin-global-var.scm: Add.
    * tests/elf-checksum/begin/global-var.sh: Add.

---
+++ tests/elf-checksum/begin-global-var.scm
@@ -0,0 +1,3 @@
+(mk-hypertrace-test
+ '((name "BEGIN probe - x = 0xDEADBEEF")
+   (in-file "begin/global-var.sh")))
---
+++ tests/elf-checksum/begin/global-var.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+DTRACE=$(which dtrace)
+SHA256SUM=$(which sha256sum)
+DIFF=$(which diff)
+
+if [ -z ${DTRACE} ] || [ -z ${SHA256SUM} ] || [ -z ${DIFF} ]
+then
+    exit 1
+fi
+
+orig_elf=$(${DTRACE} -q -E -e -s /dev/stdin <<EOF
+BEGIN {
+  x = 0xDEADBEEF;
+}
+EOF
+           )
+
+if [ -z ${orig_elf} ]
+then
+    echo "Error creating the initial ELF file."
+    exit 1
+fi
+
+new_elf=$(${DTRACE} -q -e -y ${orig_elf})
+
+if [ -z ${new_elf} ]
+then
+    echo "Error creating the second ELF file (original is ${orig_elf})"
+    exit 1
+fi
+
+orig_sha=$(${SHA256SUM} -q ${orig_elf})
+new_sha=$(${SHA256SUM} -q ${new_elf})
+
+if test ${orig_sha} = ${new_sha}
+then
+    exit 0
+else
+    echo "Checksum mismatch of ${orig_elf} and ${new_elf}"
+    exit 1
+fi
