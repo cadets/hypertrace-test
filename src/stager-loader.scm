b9c80c49f3fec771e919f81193cba22da12ae9c6
Domagoj Stolfa <ds815@gmx.com>
2021-08-06 14:56:37 +0100

    hypertrace-test: stager-loader: Prefer spaces over tabs.
    * src/stager-loader.scm: 'untabify' the file.

--- src/stager-loader.scm
+++ src/stager-loader.scm
@@ -1,10 +1,10 @@
 (declare (unit hypertrace-stager-loader))
 
 (import scheme
-	fmt
-	srfi-1
-	(chicken file)
-	(chicken pathname))
+        fmt
+        srfi-1
+        (chicken file)
+        (chicken pathname))
 
 (declare (uses hypertrace-stager))
 
@@ -26,24 +26,24 @@
   
   (let ((stager-files (glob (string-append path "*.scm"))))
     (let loop ((stager-file (car stager-files))
-	       (rest (cdr stager-files))
-	       (stagers (list)))
+               (rest (cdr stager-files))
+               (stagers (list)))
       (when (and (file-exists? stager-file)
-		 (file-readable? stager-file))
-	(let ((loaded-contents #f))
-	  (when (>= hypertrace-test-verbosity 2)
-	    (print "Loading  " stager-file))
-	  
-	  (load stager-file (lambda (x) (set! loaded-contents x)))
-	  (let ((stager (eval loaded-contents)))
-	    ;; Compute the absolute path of the test directory for this stager.
-	    (set! (hypertrace-stager-directory-path stager)
-	      (normalize-pathname (string-append
-				   hypertrace-test-dir
-				   (hypertrace-stager-directory-path stager))))
-	    
-	    (if (eq? '() rest)
-		(cons* stager stagers)
-		(loop (car rest)
-		      (cdr rest)
-		      (cons* stager stagers)))))))))
+                 (file-readable? stager-file))
+        (let ((loaded-contents #f))
+          (when (>= hypertrace-test-verbosity 2)
+            (print "Loading  " stager-file))
+          
+          (load stager-file (lambda (x) (set! loaded-contents x)))
+          (let ((stager (eval loaded-contents)))
+            ;; Compute the absolute path of the test directory for this stager.
+            (set! (hypertrace-stager-directory-path stager)
+              (normalize-pathname (string-append
+                                   hypertrace-test-dir
+                                   (hypertrace-stager-directory-path stager))))
+            
+            (if (eq? '() rest)
+                (cons* stager stagers)
+                (loop (car rest)
+                      (cdr rest)
+                      (cons* stager stagers)))))))))
