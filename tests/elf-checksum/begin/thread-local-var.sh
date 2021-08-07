7250a0a352419b25b1e76b7296ea35e22fc91c70
Domagoj Stolfa <ds815@gmx.com>
2021-08-07 17:38:46 +0100

    hypertrace-tests: tests (begin-thread-local-var): Simplify.
    * tests/elf-checksum/begin/thread-local-var.sh: Remove dependency checks.

--- tests/elf-checksum/begin/thread-local-var.sh
+++ tests/elf-checksum/begin/thread-local-var.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 
-DTRACE=$(which dtrace)
-SHA256SUM=$(which sha256sum)
-DIFF=$(which diff)
-
-if [ -z ${DTRACE} ] || [ -z ${SHA256SUM} ] || [ -z ${DIFF} ]
-then
-    exit 1
-fi
-
-orig_elf=$(${DTRACE} -q -E -e -s /dev/stdin <<EOF
+orig_elf=$(dtrace -q -E -e -s /dev/stdin <<EOF
 BEGIN {
   self->x = 0xDEADBEEF;
 }
@@ -22,7 +13,7 @@ then
     exit 1
 fi
 
-new_elf=$(${DTRACE} -q -e -y ${orig_elf})
+new_elf=$(dtrace -q -e -y ${orig_elf})
 
 if [ -z ${new_elf} ]
 then
@@ -30,8 +21,8 @@ then
     exit 1
 fi
 
-orig_sha=$(${SHA256SUM} -q ${orig_elf})
-new_sha=$(${SHA256SUM} -q ${new_elf})
+orig_sha=$(sha256sum -q ${orig_elf})
+new_sha=$(sha256sum -q ${new_elf})
 
 if test ${orig_sha} = ${new_sha}
 then
