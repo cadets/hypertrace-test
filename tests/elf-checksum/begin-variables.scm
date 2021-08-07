18a0d86ed5473cd9d8ca4e8fb062ca4296c28157
Domagoj Stolfa <ds815@gmx.com>
2021-08-07 18:00:07 +0100

    hypertrace-test: tests (begin-clause-local, begin-variables): Add.
    * tests/elf-checksum/begin-variables.scm: Add begin-clause-local and
    begin-variables.
    * tests/elf-checksum/begin/clause-local-var.sh: Add.
    * tests/elf-checksum/begin/threevars.sh: Add.

--- tests/elf-checksum/begin-variables.scm
+++ tests/elf-checksum/begin-variables.scm
@@ -1,9 +1,17 @@
 (list
  (mk-hypertrace-test
+  '((name "BEGIN probe - global/thread-local/clause-local")
+    (in-file "begin/threevars.sh")))
+
+ (mk-hypertrace-test
   '((name "BEGIN probe - self->x = 0xDEADBEEF")
     (in-file "begin/thread-local-var.sh")))
 
  (mk-hypertrace-test
- '((name "BEGIN probe - x = 0xDEADBEEF")
-   (in-file "begin/global-var.sh"))))
+  '((name "BEGIN probe - this->x = 0xDEADBEEF")
+    (in-file "begin/clause-local-var.sh")))
+ 
+ (mk-hypertrace-test
+  '((name "BEGIN probe - x = 0xDEADBEEF")
+    (in-file "begin/global-var.sh"))))
 
