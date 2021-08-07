c3aea6126942623203f128b3fce335adbff4dd89
Domagoj Stolfa <ds815@gmx.com>
2021-08-07 18:51:33 +0100

    hypertrace-test: tests (begin-variables): Add new tests.
    * tests/elf-checksum/begin-variables.scm: Add global-array and
    thread-local-array.
    * tests/elf-checksum/begin/global-array.sh: Add.
    * tests/elf-checksum/begin/thread-local-array.sh: Add.

--- tests/elf-checksum/begin-variables.scm
+++ tests/elf-checksum/begin-variables.scm
@@ -12,6 +12,14 @@
     (in-file "begin/clause-local-var.sh")))
  
  (mk-hypertrace-test
+  '((name "BEGIN probe - x[0] = 0xDEADBEEF")
+    (in-file "begin/global-array.sh")))
+
+ (mk-hypertrace-test
+  '((name "BEGIN probe - self->x[0] = 0xDEADBEEF")
+    (in-file "begin/thread-local-array.sh")))
+
+ (mk-hypertrace-test
   '((name "BEGIN probe - x = 0xDEADBEEF")
     (in-file "begin/global-var.sh"))))
 
