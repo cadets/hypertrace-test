c9d79fca838eb1b0589f6289364abbd68956747a
Domagoj Stolfa <ds815@gmx.com>
2021-08-25 19:13:29 +0100

    hypertrace-test: options: Add new option (-p, --report-path).
    * src/options.scm: Add -p, --report-path and refactor options. Also change the
    meaning for --report, -R.

--- src/options.scm
+++ src/options.scm
@@ -29,23 +29,34 @@
 (define hypertrace-options
   (list
    (args:make-option
-    (b bare)     #:none               "Bare run of the test suite")
+    (b bare)        #:none
+    "Bare run of the test suite")
    
    (args:make-option
-    (V verbose)  (optional: "LEVEL")  "Verbosity level [default: 1]"
+    (V verbose)  (optional: "LEVEL")
+    "Verbosity level [default: 1]"
     (set! arg (string->number (or arg "1"))))
 
    (args:make-option
-    (no-stagers) #:none               "Don't import any stagers.")
+    (no-stagers)    #:none
+    "Don't import any stagers.")
 
    (args:make-option
-    (use-tmpfs)  #:none               "Use tmpfs to save the report.")
+    (use-tmpfs)     #:none
+    "Use tmpfs to save the report.")
 
    (args:make-option
-    (R report)   (optional: "PATH")   "Report results from a previous run."
+    (R report)      (optional: "html|json|junit|text")
+    "Report results from a previous run (default: text)."
+    (set! arg (or arg "text")))
+
+   (args:make-option
+    (p report-path) (optional: "PATH")
+    "Path to the report file."
     (set! arg (or arg "hypertrace-test.report")))
 
    (args:make-option
-    (h help)     #:none               "Display this help text"
+    (h help)        #:none
+    "Display this help text"
     (usage))))
 
