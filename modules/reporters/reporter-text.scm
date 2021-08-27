19366e3036ecb1d2be7a6b5c627ebec4d7cc30c8
Domagoj Stolfa <ds815@gmx.com>
2021-08-27 17:29:52 +0100

    hypertrace-test: modules (reporters): Add time to text reporter.
    * modules/reporters/reporter-text.scm: Add reporting of time passed.

--- modules/reporters/reporter-text.scm
+++ modules/reporters/reporter-text.scm
@@ -1,6 +1,7 @@
 (import scheme
         fmt
-        fmt-color)
+        fmt-color
+        srfi-1)
 
 (define (generate-text-report passed failed ansi-colors?)
   ;; Report passed tests.
@@ -9,26 +10,57 @@
         (fmt #t (fmt-bold (fmt-green "PASSED tests"))
              nl (fmt-green (make-string 78 #\-)) nl)
         (fmt #t "PASSED tests"
-             nl (make-string 78 #\-) nl))
-    (for-each
-     (lambda (test-p)
-       (fmt #t (car test-p) ":  " (cadr test-p) nl))
-     passed)
-    (if ansi-colors?
-        (fmt #t (fmt-green (make-string 78 #\-)) nl)
-        (fmt #t (make-string 78 #\-) nl))
-  
-  ;; Report failed tests.
-  (when (not (equal? failed '()))
-    (if ansi-colors?
-        (fmt #t (fmt-bold (fmt-red "FAILED tests"))
-             nl (fmt-red (make-string 78 #\-)) nl)
-        (fmt #t "FAILED tests"
-             nl (make-string 78 #\-) nl))
-    (for-each
-     (lambda (test-f)
-       (fmt #t (car test-f) ":  " (cadr test-f) nl))
-     failed)
-    (if ansi-colors?
-        (fmt #t (fmt-red (make-string 78 #\-)) nl)
-        (fmt #t (make-string 78 #\-) nl)))))
+             nl (make-string 78 #\-) nl)))
+
+  ;;
+  ;; Traverse the passed tests, reporting each one and accumulating the time
+  ;; spent. If passed is '(), we won't actually do anything here.
+  ;;
+  (let ((passed-time
+         (fold
+          (lambda (test-p total-time)
+            (let ((stager (car test-p))
+                  (test   (cadr test-p))
+                  (time   (caddr test-p)))
+              (fmt #t stager ":  " test " [  " time "s  ]" nl)
+              (+ time total-time)))
+          0 passed)))
+
+    ;; Only print the time spent if we actually have tests that passed.
+    (when (not (equal? passed '()))
+      (if ansi-colors?
+          (fmt #t (fmt-green (make-string 78 #\-)) nl
+               (fmt-green "[PASSED]  ") "Time spent:  " passed-time "s" nl)
+          (fmt #t (make-string 78 #\-) nl
+               "[PASSED]  Time spent:  " passed-time "s" nl)))
+    
+    ;; Report failed tests.
+    (when (not (equal? failed '()))
+      (if ansi-colors?
+          (fmt #t (fmt-bold (fmt-red "FAILED tests"))
+               nl (fmt-red (make-string 78 #\-)) nl)
+          (fmt #t "FAILED tests"
+               nl (make-string 78 #\-) nl)))
+
+    ;;
+    ;; Traverse the failed tests, reporting each one and accumulating the time
+    ;; spent. If failed is '(), we won't actually do anything here.
+    ;;
+    (let ((failed-time
+           (fold
+            (lambda (test-f total-time)
+              (let ((stager (car test-f))
+                    (test   (cadr test-f))
+                    (time   (caddr test-f)))
+                (fmt #t stager ":  " test " [  " time "s  ]" nl)
+                (+ time total-time)))
+            0 failed)))
+      
+      ;; Only print the time spent if we actually have tests that failed.
+      (when (not (equal? failed '()))
+        (if ansi-colors?
+            (fmt #t (fmt-red (make-string 78 #\-)) nl
+                 (fmt-red "[FAILED]  ") "Time spent:  " failed-time "s" nl)
+            (fmt #t (make-string 78 #\-) nl
+                 "[FAILED]  Time spent:  " failed-time "s" nl)))
+      (fmt #t nl "Total time spent:  " (+ failed-time passed-time) "s" nl))))
